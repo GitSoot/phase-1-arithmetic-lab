7a9758fd83232a2ed412ce4007faee8757548357
Eric Onyango <eric.onyango@student.moringaschool.com>
2022-11-23 23:41:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let multiply, num1, num2;
+num1 = 31;
+num2 = 2;
+multiply = num1 * num2;
 
-// Write your code here
+let random;
+random = Math.floor(Math.random() * 100) + 1;
+
+let mod, num3, num4;
+num3 = 10;
+num4 = 6;
+
+mod = num3 % num4;
+
+let max;
+max = Math.max (1, 4, 7, 9, 20, 11);
